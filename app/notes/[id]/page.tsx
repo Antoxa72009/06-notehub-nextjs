7537ef1ee2d6a2842d9d37949dec8aff2b27331a
Anton_Stepanenko <anton_stepanenko72009@ukr.net>
2025-08-03 14:12:16 +0300

    first commit

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,28 +4,25 @@ import { fetchNoteById } from '@/lib/api/api';
 import { notFound } from 'next/navigation';
 
 interface NotesPageProps {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }
 
 export default async function NoteDetails({ params }: NotesPageProps) {
-  if (!params.id) {
-    notFound();
+  const { id } = await params;
+  if (!id) {
+    notFound(); 
   }
-
-  const noteId = params.id;
 
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ['note', noteId],
-    queryFn: () => fetchNoteById(noteId),
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NoteDetailsPage id={noteId} />
+      <NoteDetailsPage id={id} />
     </HydrationBoundary>
   );
 }
