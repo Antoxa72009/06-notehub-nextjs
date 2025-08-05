645fe16ef5a089b485fa0ee1a11c5defcd9fa943
Anton_Stepanenko <anton_stepanenko72009@ukr.net>
2025-08-05 10:02:27 +0300

    first commit

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -58,6 +58,7 @@ const Notes = ({
       totalPages: initialTotalPages,
       totalNotes: initialTotalNotes,
     },
+    placeholderData: (previousData) => previousData,
     refetchOnMount: false,
   });
 
